23818348cef2cee3cbb2ac639ef99006116e3936
nicic <nicicdamjan@gmail.com>
2019-02-05 21:44:36 +0100

    ispravka modela roomOnDisc

--- src/main/java/ftn/isa/booking/model/RoomsOnDiscount.java
+++ src/main/java/ftn/isa/booking/model/RoomsOnDiscount.java
@@ -34,7 +34,8 @@ public class RoomsOnDiscount {
 	    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	    private Room room;
 
-	  
+	    @ManyToMany(fetch = FetchType.LAZY)
+	    private Set<HotelServices> extraServices;
 
 	    public RoomsOnDiscount() {
 	    }
@@ -87,5 +88,11 @@ public class RoomsOnDiscount {
 	        this.room = room;
 	    }
 
-	
+	    public Set<HotelServices> getExtraServices() {
+	        return extraServices;
+	    }
+
+	    public void setExtraServices(Set<HotelServices> extraServices) {
+	        this.extraServices = extraServices;
+	    }
 	}
