0578f94a2683a30d61eb0dee7ff41bf3da5f9688
nicic <nicicdamjan@gmail.com>
2019-02-05 21:38:41 +0100

    create model rooms on discount

---
+++ src/main/java/ftn/isa/booking/model/RoomsOnDiscount.java
@@ -0,0 +1,91 @@
+package ftn.isa.booking.model;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class RoomsOnDiscount {
+
+	 @Id
+	    @GeneratedValue
+	    private Long id;
+
+	    @Column(name = "costValidFrom", nullable = false)
+	    private LocalDate costValidFrom;
+
+	    @Column(name = "costValidUntil", nullable = false)
+	    private LocalDate costValidUntil;
+
+	    @Column(nullable = false)
+	    private double originalCost;
+
+	    @Column(nullable = false)
+	    private double discountCost;
+
+	    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	    private Room room;
+
+	  
+
+	    public RoomsOnDiscount() {
+	    }
+
+	    public Long getId() {
+	        return id;
+	    }
+
+	    public void setId(Long id) {
+	        this.id = id;
+	    }
+
+	    public LocalDate getCostValidFrom() {
+	        return costValidFrom;
+	    }
+
+	    public void setCostValidFrom(LocalDate costValidFrom) {
+	        this.costValidFrom = costValidFrom;
+	    }
+
+	    public LocalDate getCostValidUntil() {
+	        return costValidUntil;
+	    }
+
+	    public void setCostValidUntil(LocalDate costValidUntil) {
+	        this.costValidUntil = costValidUntil;
+	    }
+
+	    public double getOriginalCost() {
+	        return originalCost;
+	    }
+
+	    public void setOriginalCost(double originalCost) {
+	        this.originalCost = originalCost;
+	    }
+
+	    public double getDiscountCost() {
+	        return discountCost;
+	    }
+
+	    public void setDiscountCost(double discountCost) {
+	        this.discountCost = discountCost;
+	    }
+
+	    public Room getRoom() {
+	        return room;
+	    }
+
+	    public void setRoom(Room room) {
+	        this.room = room;
+	    }
+
+	
+	}
