068ffabe3d3cd6e08a1cfa7a35f867cd18329c31
nicic <nicicdamjan@gmail.com>
2019-02-06 22:35:53 +0100

    repozitorijum za sobe na popustu

---
+++ src/main/java/ftn/isa/booking/reporistory/RoomsOnDiscount.java
@@ -0,0 +1,7 @@
+package ftn.isa.booking.reporistory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomsOnDiscount extends JpaRepository<RoomsOnDiscount,Long> {
+
+}
