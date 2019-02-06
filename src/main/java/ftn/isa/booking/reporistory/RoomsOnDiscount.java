65aac2e69895ede893e6af4351e72b97764434c5
nicic <nicicdamjan@gmail.com>
2019-02-06 23:25:49 +0100

    change name Rep

--- src/main/java/ftn/isa/booking/reporistory/RoomsOnDiscount.java
+++
@@ -1,7 +0,0 @@
-package ftn.isa.booking.reporistory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomsOnDiscount extends JpaRepository<RoomsOnDiscount,Long> {
-
-}
---
+++ src/main/java/ftn/isa/booking/reporistory/RoomsOnDiscountRepository.java
@@ -0,0 +1,9 @@
+package ftn.isa.booking.reporistory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ftn.isa.booking.model.RoomsOnDiscount;
+
+public interface RoomsOnDiscountRepository extends JpaRepository<RoomsOnDiscount,Long> {
+
+}
