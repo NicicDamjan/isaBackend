16c990168e292a2095f01a9f07a3caa5f2568bdf
LosBlancos6 <stefans996@gmail.com>
2019-02-07 01:46:33 +0100

    izmenjen flightReservation service

--- src/main/java/ftn/isa/booking/services/FlightReservationService.java
+++ src/main/java/ftn/isa/booking/services/FlightReservationService.java
@@ -6,12 +6,17 @@ import org.springframework.stereotype.Service;
 import ftn.isa.booking.model.Flight;
 import ftn.isa.booking.model.FlightReservation;
 import ftn.isa.booking.model.Seat;
+import ftn.isa.booking.model.SeatTicket;
 import ftn.isa.booking.model.User;
 import ftn.isa.booking.reporistory.DestinationRepository;
 import ftn.isa.booking.reporistory.FlightRepository;
 import ftn.isa.booking.reporistory.FlightReservationRepository;
 import ftn.isa.booking.reporistory.SeatRepository;
 import ftn.isa.booking.reporistory.UsersRepository;
+import ftn.isa.booking.reporistory.SeatTicketRepository;
+import ftn.isa.booking.controller.airCompanyController.dto.FlightReservationDTO;
+
+
 
 @Service
 public class FlightReservationService {
@@ -28,10 +33,38 @@ public class FlightReservationService {
 	@Autowired
 	private SeatService seatService;
 	
+	@Autowired
+	private SeatTicketRepository seatTicketRepository;
 	
 	
-	
-	
+	public FlightReservation reserveFlight(FlightReservationDTO fd) {
+
+		FlightReservation fr = new FlightReservation();
+		
+		User u = userRepository.findOneByEmail(fd.getEmail());
+		
+		Flight f = flightRepository.findById(fd.getFlightId()).get();
+		
+		Seat s = seatService.changeTypeSeat(fd.getSeatId());
+		
+		fr.setUser(u);
+		fr.setFlight(f);
+		fr.setPrice(fd.getPrice());
+		
+		SeatTicket st = new SeatTicket();
+		
+		st.setSeat(s);
+		st.setPassport(fd.getPassport());
+		
+		fr.getSeatTickets().add(seatTicketRepository.save(st));
+		
+		fr.setPrice(fd.getPrice());
+		
+		//fr.getSeats().add(s);
+		
+		return flightResRepository.save(fr);
+
+	}
 	public FlightReservation findById(Long id){
 		return flightResRepository.findById(id).get();
 	}
